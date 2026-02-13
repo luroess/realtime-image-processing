2dc4119445e533636060de26542755dc26fe1e23
ValentinBumeder <valentinbumeder@gmail.com>
2026-02-13 19:23:14 +0000

    Merge reset of debouncing

--- testbench/tests/test_debouncing.py
+++ testbench/tests/test_debouncing.py
@@ -1,22 +1,39 @@
+""" Testbench for VHDL Entity Debouncer """
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer
 
+CLK_PERIOD_NS = 10           # 100 MHz
+DEBOUNCE_MS = 10
+DEBOUNCE_NS = DEBOUNCE_MS * 1_000_000
+
+async def apply_reset(dut, cycles: int = 5) -> None:
+    """Apply Reset"""
+    dut.i_rst.value = 1
+    dut.i_btn.value = 0
+
+    for _ in range(cycles):
+        await RisingEdge(dut.i_clk)
+
+    dut.i_rst.value = 0
+    await RisingEdge(dut.i_clk)
+
+    await check_debounced(dut, 0, 10)
+
 
 async def check_debounced(dut, expected, duration_ns):
-    """
-    Wait duration_ns and check that the debounced output is stable.
-    """
+    """ Wait duration_ns and check that the debounced output is stable. """
     if duration_ns != 0:
         await Timer(duration_ns, unit="ns")
     if dut.o_btn_debounced.value != expected:
-        raise Exception(
+        raise AssertionError(
             f"Debounced output mismatch! Expected {expected}, got {int(dut.o_btn_debounced.value)}"
         )
 
 async def set_i_btn_value_and_wait(dut, i_btn_value, wait_duration, wait_duration_unit='ns'):
+    """ Set input button value and wait for defined time. """
     dut.i_btn.value = i_btn_value
-    
+
     if wait_duration != 0:
         await Timer(wait_duration, unit=wait_duration_unit)
 
@@ -26,16 +43,10 @@ async def debouncer_test(dut):
     """Cocotb testbench for Debouncer with automatic checking"""
 
     # --------------------------------------------------
-    # Parameters
-    # --------------------------------------------------
-    CLK_PERIOD_NS = 10           # 100 MHz
-    DEBOUNCE_MS = 10
-    DEBOUNCE_NS = DEBOUNCE_MS * 1_000_000
-
-    # --------------------------------------------------
     # Clock generation
     # --------------------------------------------------
     cocotb.start_soon(Clock(dut.i_clk, CLK_PERIOD_NS, unit="ns").start())
+    await apply_reset(dut)
 
     # --------------------------------------------------
     # Initial values
@@ -80,9 +91,3 @@ async def debouncer_test(dut):
     # Now stable released
     await set_i_btn_value_and_wait(dut, 0, 0)
     await check_debounced(dut, 0, DEBOUNCE_NS + 1_000_000)  # 11 ms
-
-    # --------------------------------------------------
-    # End simulation
-    # --------------------------------------------------
-    await Timer(100, unit="ns")
-    dut._log.info("Simulation completed successfully")
