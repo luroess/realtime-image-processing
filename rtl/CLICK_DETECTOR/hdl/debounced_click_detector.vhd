2a0850597ebd7bae8e96ad1bd154ff6c8970ab9e
ValentinBumeder <valentinbumeder@gmail.com>
2026-02-16 14:27:36 +0000

    test: Added testbench for DebouncedClickDetector

--- rtl/CLICK_DETECTOR/hdl/debounced_click_detector.vhd
+++ rtl/CLICK_DETECTOR/hdl/debounced_click_detector.vhd
@@ -1,22 +1,19 @@
 library ieee;
   use ieee.std_logic_1164.all;
 
-  -- TODO(@ValentinBumeder): Please write a testbench for this module.
-
 entity DebouncedClickDetector is
   generic (
     G_CLK_FREQ_HZ    : integer := 100_000_000; -- 100 MHz
-    G_DEBOUNCE_NS    : integer := 10_000_000;  -- 10 ms
-    G_CLICK_TIMER_NS : integer := 500_000_000  -- 0.5 s
+    G_DEBOUNCE_NS    : integer := 10_000_000  -- 10 ms
   );
   port (
     i_clk           : in  std_logic;
-    i_rst           : in  std_logic;
+    i_rst_n           : in  std_logic;
     i_btn           : in  std_logic;
     o_btn_debounced : out std_logic;
-    o_single_click  : out std_logic;
-    o_double_click  : out std_logic;
-    o_triple_click  : out std_logic
+    o_pass_grayscale  : out std_logic;
+    o_pass_lowpass_filter  : out std_logic;
+    o_pass_sobel  : out std_logic
   );
 end entity;
 
@@ -30,22 +27,19 @@ begin
     G_CLK_FREQ_HZ => G_CLK_FREQ_HZ,
     G_DEBOUNCE_NS => G_DEBOUNCE_NS
   ) port map (
-    i_rst           => i_rst,
+    i_rst_n           => i_rst_n,
     i_clk           => i_clk,
     i_btn           => i_btn,
     o_btn_debounced => s_btn_debounced
   );
 
-  U_ClickDetector: entity work.ClickDetector(A_Rtl) generic map (
-    G_CLK_FREQ_HZ    => G_CLK_FREQ_HZ,
-    G_CLICK_TIMER_NS => G_CLICK_TIMER_NS
-  ) port map (
+  U_ClickDetector: entity work.ClickDetector(A_Rtl) port map (
     i_clk           => i_clk,
-    i_rst           => i_rst,
+    i_rst_n           => i_rst_n,
     i_btn_debounced => s_btn_debounced,
-    o_single_click  => o_single_click,
-    o_double_click  => o_double_click,
-    o_triple_click  => o_triple_click
+    o_pass_grayscale => o_pass_grayscale,
+    o_pass_lowpass_filter  => o_pass_lowpass_filter,
+    o_pass_sobel  => o_pass_sobel
   );
 
   o_btn_debounced <= s_btn_debounced;
---
+++ testbench/tests/test_debounced_click_detector.py
@@ -0,0 +1,80 @@
+"""Test layer: Execute combined test for debouncer and click detection."""
+
+from __future__ import annotations
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+from cocotb.utils import get_sim_time
+
+from drivers.debouncing_driver import DebouncingDriver
+from drivers.click_detection_driver import ClickDetectionDriver
+
+
+# CONSTANTS
+CLK_PERIOD_NS = 10  # 100 MHz
+CLK_TIMER_NS = 100  # 100 ns (10 cycles) for simulation
+
+@cocotb.test()
+async def test_debounced_click_detection(dut) -> None:
+    """Test debounced Click Detection."""
+    i_clk = getattr(dut, "i_clk")
+    i_rst = getattr(dut, 'i_rst_n')
+    debouncing_driver = DebouncingDriver(dut)
+    click_detection_driver = ClickDetectionDriver(dut)
+
+
+    # --------------------------------------------------
+    # Reset
+    # --------------------------------------------------
+
+    cocotb.start_soon(Clock(dut.i_clk, CLK_PERIOD_NS, unit="ns").start())
+    await debouncing_driver.apply_reset()
+
+    # --------------------------------------------------
+    # Transition to ST_GRAYSCALE
+    # --------------------------------------------------
+    print(f"Transition to state ST_GRAYSCALE")
+    await debouncing_driver.simulate_bouncing(0)
+    await click_detection_driver.check_output(1, 1, 1, 0)
+    await debouncing_driver.set_i_btn_value_and_wait(1, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 1, 1, 20)
+    await debouncing_driver.simulate_bouncing(1)
+    await debouncing_driver.set_i_btn_value_and_wait(0, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 1, 1, 20)
+
+    # # --------------------------------------------------
+    # # Transition to ST_LOWPASS
+    # # --------------------------------------------------
+    print(f"Transition to state ST_LOWPASS")
+    await debouncing_driver.simulate_bouncing(0)
+    await click_detection_driver.check_output(0, 1, 1, 0)
+    await debouncing_driver.set_i_btn_value_and_wait(1, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 0, 1, 20)
+    await debouncing_driver.simulate_bouncing(1)
+    await debouncing_driver.set_i_btn_value_and_wait(0, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 0, 1, 20)
+
+    # # --------------------------------------------------
+    # # Transition to ST_SOBEL
+    # # --------------------------------------------------
+    print(f"Transition to state ST_SOBEL")
+    await debouncing_driver.simulate_bouncing(0)
+    await click_detection_driver.check_output(0, 0, 1, 0)
+    await debouncing_driver.set_i_btn_value_and_wait(1, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 0, 0, 20)
+    await debouncing_driver.simulate_bouncing(1)
+    await debouncing_driver.set_i_btn_value_and_wait(0, CLK_TIMER_NS)
+    await click_detection_driver.check_output(0, 0, 0, 20)
+
+    # # --------------------------------------------------
+    # # Transition to ST_PASSTHROUGH
+    # # --------------------------------------------------
+    print(f"Transition to state ST_PASSTHROUGH")
+    await debouncing_driver.simulate_bouncing(0)
+    await click_detection_driver.check_output(0, 0, 0, 0)
+    await debouncing_driver.set_i_btn_value_and_wait(1, CLK_TIMER_NS)
+    await click_detection_driver.check_output(1, 1, 1, 20)
+    await debouncing_driver.simulate_bouncing(1)
+    await debouncing_driver.set_i_btn_value_and_wait(0, CLK_TIMER_NS)
+    await click_detection_driver.check_output(1, 1, 1, 20)
