1346bb2ad4432ce55ded281020f122e39e8724ae
JanDuchscherer104 <janduchscher@gmail.com>
2026-02-19 09:34:27 +0100

    feat(click-det): update output ctrl ports

--- rtl/CLICK_DETECTOR/hdl/click_detection.vhd
+++ rtl/CLICK_DETECTOR/hdl/click_detection.vhd
@@ -8,11 +8,12 @@ entity ClickDetector is
     i_rst_n             : in  std_logic;
     i_btn_debounced     : in  std_logic;                   -- BTN1: processing state FSM
     i_btn2_debounced    : in  std_logic;                   -- BTN2: base image state FSM
+
     o_pass_grayscale    : out std_logic;
     o_pass_blurr_filter : out std_logic;
     o_pass_sobel        : out std_logic;
     o_pass_fast         : out std_logic;
-    o_base_mode         : out std_logic_vector(1 downto 0);
+    o_overlay_zeros     : out std_logic;                   -- Overlay zeros on the output when base image is disabled
     o_led               : out std_logic_vector(3 downto 0) -- 4 LEDs
   );
 end entity;
@@ -57,8 +58,7 @@ begin
     s_next_state <= s_current_state;
 
     -- Processing chain is RGB->GRAY->BLUR->SOBEL/FAST.
-    -- Base grayscale stage stays enabled in all BTN1 states.
-    o_pass_grayscale <= '0';
+    -- Base-image selection is controlled by BTN2 base-state FSM.
     o_pass_blurr_filter <= '1';
     o_pass_sobel <= '1';
     o_pass_fast <= '1';
@@ -111,27 +111,32 @@ begin
   P_COMB_BASE_FSM: process (s_base_current_state, s_btn2_prev, i_btn2_debounced, s_current_state)
   begin
     s_base_next_state <= s_base_current_state;
-    o_base_mode <= "00";
+    o_overlay_zeros <= '1';
+    o_pass_grayscale <= '0';
 
     -- For ST_PASS_ALL and ST_BLUR the base image is not used.
     -- Keep base mode fixed to ZEROS as requested.
     if (s_current_state = ST_PASS_ALL) or (s_current_state = ST_BLUR) then
       s_base_next_state <= ST_ZEROS;
-      o_base_mode <= "00";
+      o_overlay_zeros <= '1';
+      o_pass_grayscale <= '0';
     else
       case s_base_current_state is
         when ST_BRAM_RGB =>
-          o_base_mode <= "01";
+          o_overlay_zeros <= '0';
+          o_pass_grayscale <= '1';
           if i_btn2_debounced = '1' and s_btn2_prev = '0' then
             s_base_next_state <= ST_BRAM_GRAY;
           end if;
         when ST_BRAM_GRAY =>
-          o_base_mode <= "10";
+          o_overlay_zeros <= '0';
+          o_pass_grayscale <= '0';
           if i_btn2_debounced = '1' and s_btn2_prev = '0' then
             s_base_next_state <= ST_ZEROS;
           end if;
         when ST_ZEROS =>
-          o_base_mode <= "00";
+          o_overlay_zeros <= '1';
+          o_pass_grayscale <= '0';
           if i_btn2_debounced = '1' and s_btn2_prev = '0' then
             s_base_next_state <= ST_BRAM_RGB;
           end if;
